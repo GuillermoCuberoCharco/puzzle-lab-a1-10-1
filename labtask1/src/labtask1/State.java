667c1961a20017a4700473bf34f5e8177d299c11
Fluctlights <antoniofernando.mateo@alu.uclm.es>
2021-10-20 21:37:11 +0200

    Cambios en successorFN()

--- labtask1/src/labtask1/State.java
+++ labtask1/src/labtask1/State.java
@@ -44,21 +44,23 @@ public class State {
 	}
 
 	//Saca todos los posibles resultados de hacer el movimiento con quantity sobre todas as botellas entre si
-	private ArrayList<Action> successorFN(State init_State, int quantity) {
+	private ArrayList<Action> successorFN(State init_State) {
 		ArrayList<Action> New_actions = new ArrayList<Action>();
 		State new_state = new State();	
 
-		for(int i=0; i<init_State.getBottles().size(); i++) {
-			for(int j=0; j<init_State.getBottles().size(); i++) {
+		for(int i=0; i<init_State.getBottles().size()-1; i++) {
+			int quantity = init_State.getBottles().get(i).getQuantity();
+
+			for(int j=0; j<init_State.getBottles().size()-1; j++) {
 				
 				new_state.setBottles(init_State.getBottles());
-				
+
 				if(i!=j) {
 					
-					if(quantity > 0 && Is_PossibleAction(init_State.getBottles().get(i), init_State.getBottles().get(j), quantity)) {			
+					if( quantity > 0 && Is_PossibleAction(init_State.getBottles().get(i), init_State.getBottles().get(j), quantity)) {			
 						new_state.getBottles().get(i).moveLiquid(quantity, new_state.getBottles().get(j));
 						
-						Action actionPerformed = new Action(i, j, quantity);
+						Action actionPerformed = new Action(i, j, init_State.getBottles().get(i).getQuantity());
 						actionPerformed.setResult(new_state); 
 						/* he tenido que guardar el new_state en la action 
 						porque si no no sabia como devolver action y el nuevo state a la vez */
